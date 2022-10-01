bdcaf6ca2455abc6df6688be2f86747e6382c60c
Jess <Jessjo.324@gmail.com>
2022-10-01 00:20:36 -0500

    Agregando la lista de las películas en preventa

--- unicine/unicine_front/lib/ui/shared/custom_labels.dart
+++ unicine/unicine_front/lib/ui/shared/custom_labels.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 class CustomLabels {
 
   static TextStyle h1 = const TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle h2 = const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
   static TextStyle h3 = const TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
   static TextStyle h3n = const TextStyle(fontSize: 15, fontWeight: FontWeight.normal);
   static TextStyle h4 = const TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
---
+++ unicine/unicine_front/lib/ui/views/unicine/billboard/list_movies_scroll.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:uni_cine/ui/views/unicine/billboard/image_box.dart';
+
+class ListMoviesScroll extends StatelessWidget {
+  final String img;
+  final double? width;
+  final String? duration;
+
+  const ListMoviesScroll(
+      {Key? key,
+      required this.movies,
+      required this.img,
+      this.width,
+      this.duration})
+      : super(key: key);
+
+  final int movies;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 360,
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+          },
+        ),
+        child: ListView.builder(
+            physics: const AlwaysScrollableScrollPhysics(),
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemCount: movies,
+            itemBuilder: (context, movies) =>
+                ImageBox(img: img, width: width, duration: duration)),
+      ),
+    );
+  }
+}
