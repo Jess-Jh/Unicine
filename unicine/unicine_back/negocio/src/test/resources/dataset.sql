cb5c4bac6239e611c7f39f5074417abfb72a5d41
JoseJuanjo13 <juanj.hincapieo@uqvirtual.edu.co>
2022-11-17 23:00:14 -0500

    se modifica dataset

--- unicine/unicine_back/negocio/src/test/resources/dataset.sql
+++ unicine/unicine_back/negocio/src/test/resources/dataset.sql
@@ -4,12 +4,27 @@ insert into ciudad values (3, "manizales");
 insert into ciudad values (4, "cali");
 insert into ciudad values (5, "bogota");
 
-insert into cliente values ("12345", "123", "correo@gmail.com", 1, "Cliente", "jessica ospina", 1, "ruta/img", 1, 1);
-insert into cliente values ("12346", "123", "correo2@gmail.com", 1, "Cliente", "johana echeverry", 1, "ruta/img", 1, 1);
-insert into cliente values ("12347", "123", "correo3@gmail.com", 1, "Cliente", "juan jose ortiz", 1, "ruta/img", 1, 1);
-insert into cliente values ("12348", "123", "correo4@gmail.com", 1, "Cliente", "david rodriguez", 1, "ruta/img", 1, 1);
-insert into cliente values ("12349", "123", "correo5@gmail.com", 1, "Cliente", "alison lopez", 1, "ruta/img", 1, 1);
-insert into cliente values ("12353", "123", "correo6@gmail.com", 1, "Cliente", "jose daniel", 1, "ruta/img", 1, 1);
+insert into rol values ("1", "CLIENTE");
+insert into rol values ("2", "ADMIN");
+insert into rol values ("3", "ADMIN_TEATRO");
+
+insert into cliente values ("12345", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "correo@gmail.com", 1, "Cliente", "jessica ospina", 1, "ruta/img", 1, 1, 1);
+insert into cliente values ("12346", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "correo2@gmail.com", 1, "Cliente", "johana echeverry", 1, "ruta/img", 1, 1, 1);
+insert into cliente values ("12347", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "correo3@gmail.com", 1, "Cliente", "juan jose ortiz", 1, "ruta/img", 1, 1, 1);
+insert into cliente values ("12348", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "correo4@gmail.com", 1, "Cliente", "david rodriguez", 1, "ruta/img", 1, 1, 1);
+insert into cliente values ("12349", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "correo5@gmail.com", 1, "Cliente", "alison lopez", 1, "ruta/img", 1, 1, 1);
+insert into cliente values ("12353", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "correo6@gmail.com", 1, "Cliente", "jose daniel", 1, "ruta/img", 1, 1, 1);
+
+insert into cliente values ("1094973940", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "daniela@gmail.com", 1, "Cliente", "Daniela gonzales", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/10_lo3urz.jpg", 1, 1, 1);
+insert into cliente values ("1094973941", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "diegoA@gmail.com", 1, "Cliente", "Diego alejandro", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/7_q2mgd7.png", 1, 1, 1);
+insert into cliente values ("1094973943", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "erika@gmail.com", 1, "Cliente", "Erika paola", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/9_b8orrx.jpg", 1, 1, 1);
+insert into cliente values ("1094973944", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "nicop@gmail.com", 1, "Cliente", "Nico pereira", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/8_h9xnaj.jpg", 1, 1, 1);
+insert into cliente values ("1094973945", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "Saula@gmail.com", 1, "Cliente", "Saul andres", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/3_qigmpy.jpg", 1, 1, 1);
+insert into cliente values ("1094973946", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "jaime@gmail.com", 1, "Cliente", "Jaime ochoa", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/1_mbdpcc.jpg", 1, 1, 1);
+insert into cliente values ("1094973947", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "Andrea@gmail.com", 1, "Cliente", "Andrea gimenez", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/4_yfpvcl.jpg", 1, 1, 1);
+insert into cliente values ("1094973948", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "jordic@gmail.com", 1, "Cliente", "Jordi cazali", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/2_w4hqhw.jpg", 1, 1, 1);
+insert into cliente values ("1094973949", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "paoa@gmail.com", 1, "Cliente", "Paola andrea", 1, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708907/unicine/Usuarios/6_tqel5d.jpg", 1, 1, 1);
+
 
 insert into cliente_telefonos values ("12345", "3137705899");
 insert into cliente_telefonos values ("12345", "3137705800");
@@ -19,19 +34,37 @@ insert into cliente_telefonos values ("12348", "3117895588");
 insert into cliente_telefonos values ("12349", "3789988564");
 insert into cliente_telefonos values ("12353", "3889745523");
 
-insert into administrador values ("12350", "123", "admin1@gmail.com", 2, "Administrador", "administrador 1", 1);
-insert into administrador values ("12351", "123", "admin2@gmail.com", 2, "Administrador", "administrador 2", 1);
-insert into administrador values ("12352", "123", "admin3@gmail.com", 2, "Administrador", "administrador 3", 1);
-insert into administrador values ("12354", "123", "admin4@gmail.com", 2, "Administrador", "administrador 4", 1);
-insert into administrador values ("12355", "123", "admin5@gmail.com", 2, "Administrador", "administrador 5", 1);
+insert into cliente_telefonos values ("1094973940", "3118779850");
+insert into cliente_telefonos values ("1094973941", "3118779851");
+insert into cliente_telefonos values ("1094973949", "3118779852");
+insert into cliente_telefonos values ("1094973943", "3118779853");
+insert into cliente_telefonos values ("1094973944", "3118779854");
+insert into cliente_telefonos values ("1094973945", "3118779855");
+insert into cliente_telefonos values ("1094973946", "3118779856");
+insert into cliente_telefonos values ("1094973947", "3118779857");
+insert into cliente_telefonos values ("1094973948", "3118779858");
 
-insert into administrador values ("12356", "123", "admin6@gmail.com", 3, "Administrador_Teatro", "administrador 6", 1);
+
+insert into administrador values ("12350", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "admin1@gmail.com", 2, "Administrador", "administrador 1", 2, 1);
+insert into administrador values ("12351", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "admin2@gmail.com", 2, "Administrador", "administrador 2", 2, 1);
+insert into administrador values ("12352", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "admin3@gmail.com", 2, "Administrador_Teatro", "administrador 3", 3, 1);
+insert into administrador values ("12354", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "admin4@gmail.com", 2, "Administrador", "administrador 4", 2, 1);
+insert into administrador values ("12355", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "admin5@gmail.com", 2, "Administrador_Teatro", "administrador 5", 3, 1);
+insert into administrador values ("12356", "$2a$10$/W/kXAOQZWzRA6b/bybVjOmWV5x.RgA2hQVz4Z7AhGwmRZpDaKb86", "admin6@gmail.com", 3, "Administrador_Teatro", "administrador 6", 3, 1);
 
 insert into confiteria values (1, "ruta/img", "confiteria 1", 13000.0);
 insert into confiteria values (2, "ruta/img2", "confiteria 2", 14000.0);
 insert into confiteria values (3, "ruta/img3", "confiteria 3", 15000.0);
 insert into confiteria values (4, "ruta/img4", "confiteria 4", 16000.0);
 insert into confiteria values (5, "ruta/img5", "confiteria 5", 17000.0);
+
+insert into confiteria values (6, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758824/unicine/confiteria/combo2_ogfaaz.png", "Combo 2", 25000.0);
+insert into confiteria values (7, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758822/unicine/confiteria/palomitas_nnmn3r.webp", "Palomitas", 12000.0);
+insert into confiteria values (8, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758821/unicine/confiteria/nachos_abmshd.png", "Nachos", 15500.0);
+insert into confiteria values (9, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758818/unicine/confiteria/gaseosa_zlhj17.jpg", "Gaseosa coca-cola", 9500.0);
+insert into confiteria values (10, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758817/unicine/confiteria/combo1_gmon0k.jpg", "Combo 1", 25000.0);
+insert into confiteria values (11, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758817/unicine/confiteria/perrocaliente_ah50sp.jpg", "Perro caliente", 13000.0);
+insert into confiteria values (12, "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667758814/unicine/confiteria/combo3_l1dprc.jpg", "Combo 3", 12000.0);
 
 
 insert into PQRS values (1, "Mensaje prueba pqrs aaa", "12345");
@@ -40,11 +73,23 @@ insert into PQRS values (3, "Mensaje prueba pqrs ccc", "12347");
 insert into PQRS values (4, "Mensaje prueba pqrs ccc", "12348");
 insert into PQRS values (5, "Mensaje prueba pqrs ccc", "12349");
 
-insert into pelicula values (1, "thriller", "ruta/img", "pelicula prueba", "jhon doe - jana doe", "sinopsis prueba", "ruta/url", 1);
-insert into pelicula values (2, "thriller2", "ruta/img2", "pelicula prueba2", "jhon doe2 - jana doe2", "sinopsis prueba2", "ruta/url2", 1);
-insert into pelicula values (3, "thriller3", "ruta/img3", "pelicula prueba3", "jhon doe3 - jana doe3", "sinopsis prueba3", "ruta/url3", 2);
-insert into pelicula values (4, "thriller4", "ruta/img4", "pelicula prueba4", "jhon doe4 - jana doe4", "sinopsis prueba4", "ruta/url4", 1);
-insert into pelicula values (5, "thriller5", "ruta/img5", "pelicula prueba5", "jhon doe5 - jana doe5", "sinopsis prueba5", "ruta/url5", 1);
+insert into pelicula values (1, "CARTELERA", "thriller", "ruta/img", "pelicula prueba", "jhon doe - jana doe", "sinopsis prueba", "ruta/url");
+insert into pelicula values (2, "CARTELERA", "thriller2", "ruta/img2", "pelicula prueba2", "jhon doe2 - jana doe2", "sinopsis prueba2", "ruta/url2");
+insert into pelicula values (3, "PREVENTA", "thriller3", "ruta/img3", "pelicula prueba3", "jhon doe3 - jana doe3", "sinopsis prueba3", "ruta/url3");
+insert into pelicula values (4, "CARTELERA", "thriller4", "ruta/img4", "pelicula prueba4", "jhon doe4 - jana doe4", "sinopsis prueba4", "ruta/url4");
+insert into pelicula values (5, "PREVENTA", "thriller5", "ruta/img5", "pelicula prueba5", "jhon doe5 - jana doe5", "sinopsis prueba5", "ruta/url5");
+
+insert into pelicula values (6, "CARTELERA", "Accion", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708707/unicine/Peliculas/batman_hj82mv.jpg", "Batman: el caballero de la noche", "Christian Bale - Michael Caine - Heath Ledger - Gary Oldman - Morgan Freeman", "Batman tiene que mantener el equilibrio entre el heroísmo y el vigilantismo para pelear contra un vil criminal conocido como el Guasón, que pretende sumir Ciudad Gótica en la anarquía.", "https://youtu.be/LDG9bisJEaI");
+insert into pelicula values (7, "CARTELERA", "Comedia", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708707/unicine/Peliculas/zombieland_mdbczt.jpg", "Tierra de Zombies", "	Woody Harrelson - Jesse Eisenberg - Emma Stone", "Después de que un virus transforma a la mayoría de las personas en zombis", "https://youtu.be/thu_n86IhDY");
+insert into pelicula values (8, "CARTELERA", "Psicologico", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708706/unicine/Peliculas/joker_hfzrvp.jpg", "Joker", "Joaquin Phoenix - Robert De Niro - Zazie Beetz", "Arthur Fleck adora hacer reír a la gente, pero su carrera como comediante es un fracaso. El repudio social, la marginación y una serie de trágicos acontecimientos lo conducen por el sendero de la locura y, finalmente, cae en el mundo del crimen.", "https://youtu.be/WwkZqhpE0E4");
+insert into pelicula values (9, "PREVENTA", "Accion", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708705/unicine/Peliculas/morbius_knzc98.jpg", "Morbius", "Jared Leto - Matt Smith - Adria Arjona", "Al intentar encontrar una cura para una extraña enfermedad en su sangre, un doctor se transforma en un vampiro.", "https://youtu.be/SYOePdCZfqM");
+insert into pelicula values (10, "CARTELERA", "Accion", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708704/unicine/Peliculas/madmax_rwtpua.jpg", "Mad Max: furia en el camino", "Tom Hardy - Charlize Theron - Nicholas Hoult", "Aunque está decidido a vagar solo por el páramo post-apocalíptico, Mad Max se une a Furiosa, una comandante fugitiva, y su banda, quienes están tratando de escapar de un señor de la guerra.", "https://youtu.be/3KMMdqfwvmc");
+insert into pelicula values (11, "PREVENTA", "Accion", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708704/unicine/Peliculas/pulp_fiction_anblw9.jpg", "Pulp Fiction", "John Travolta - Samuel L. Jackson - Uma Thurman", "La vida de un boxeador, dos sicarios, la esposa de un gánster y dos bandidos se entrelaza en una historia de violencia y redención.", "https://youtu.be/tGpTpVyI_OQ");
+insert into pelicula values (12, "PREVENTA", "Accion", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708704/unicine/Peliculas/spiderman_cxnnsl.jpg", "Spider-Man", "	Tobey Maguire - Willem Dafoe - Kirsten Dunst", "Luego de sufrir la picadura de una araña genéticamente modificada, un estudiante de secundaria tímido y torpe adquiere increíbles capacidades como arácnido.", "https://youtu.be/t06RUxPbp_c");
+insert into pelicula values (13, "CARTELERA", "Terror", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708703/unicine/Peliculas/pesadilla_pimd8j.jpg", "Pesadilla en la calle Elm", "Heather Langenkamp - Ronee Blakley - John Saxon", "Un grupo de adolescentes sufre unas pesadillas horrendas en las que un ser deforme que porta garras de acero los persigue. Lo más inquietante es que los hechos empiezan a sugerir que lo que ocurre mientras sueñan repercute en la vida real.", "https://youtu.be/aBhok4ziOU4");
+insert into pelicula values (14, "PREVENTA", "Terror", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708702/unicine/Peliculas/halloween_pm1hky.jpg", "Halloween II", "Jamie Lee Curtis - Donald Pleasence", "El asesino Michael Myers sigue a un blanco herido hasta el hospital el 31 de octubre en Haddonfield, Illinois.", "https://youtu.be/N2dN_3Y-3bU");
+insert into pelicula values (15, "PREVENTA", "Accion", "https://res.cloudinary.com/ddsltuhyg/image/upload/v1667708702/unicine/Peliculas/transformers_cgkvlf.jpg", "Transformers", "Shia LaBeouf - Megan Fox - Tyrese Gibson", "El destino de la humanidad está en juego cuando dos razas de robots, los buenos Autobots y los villanos Decepticons, llevan su guerra a la Tierra.", "https://youtu.be/IBCLUy7pB5w");
+
 
 insert into cupon values (1, "criterio prueba", "descripcion prueba", "2022-10-02", 20000.0);
 insert into cupon values (2, "criterio prueba2", "descripcion prueba2", "2022-10-02", 21000.0);
@@ -70,11 +115,15 @@ insert into teatro values (3, "Cra. 14 #8-02", "Teatro 3 prueba", 1);
 insert into teatro values (4, "Cra. 14 #9-02", "Teatro 4 prueba", 1);
 insert into teatro values (5, "Cra. 14 #10-02", "Teatro 5 prueba", 1);
 
-insert into sala values (1, "Sala 1 prueba", 1);
-insert into sala values (2, "Sala 2 prueba", 1);
-insert into sala values (3, "Sala 3 prueba", 2);
-insert into sala values (4, "Sala 4 prueba", 2);
-insert into sala values (5, "Sala 5 prueba", 3);
+insert into distribucion_silla values (1, 20, "Distribuccion 3", 12, 236);
+insert into distribucion_silla values (2, 31, "Distribuccion 1", 17, 519);
+insert into distribucion_silla values (3, 20, "Distribuccion 2", 11, 220);
+
+insert into sala values (1, "Sala 1 prueba", 1, 1);
+insert into sala values (2, "Sala 2 prueba", 1, 2);
+insert into sala values (3, "Sala 3 prueba", 2, 1);
+insert into sala values (4, "Sala 4 prueba", 2, 3);
+insert into sala values (5, "Sala 5 prueba", 3, 1);
 
 insert into funcion_sala values (1, 2, 2, 1);
 insert into funcion_sala values (2, 1, 1, 1);
@@ -100,14 +149,8 @@ insert into confiteria_compra values (3, 40000.0, 1, 2, 3);
 insert into confiteria_compra values (4, 70000.0, 1, 5, 4);
 insert into confiteria_compra values (5, 60000.0, 1, 4, 5);
 
-insert into entrada values (1, "Columna A1", "Fila C3", 20000.0, 1);
-insert into entrada values (2, "Columna A2", "Fila J3", 20000.0, 1);
-insert into entrada values (3, "Columna A1", "Fila C4", 20000.0, 2);
-insert into entrada values (4, "Columna A1", "Fila B3", 20000.0, 3);
-insert into entrada values (5, "Columna A3", "Fila B4", 20000.0, 3);
-
-insert into distribucion_silla values (3, 9, "Distribuccion 3", 9, 81, 1);
-insert into distribucion_silla values (1, 7, "Distribuccion 1", 7, 49, 1);
-insert into distribucion_silla values (2, 8, "Distribuccion 2", 8, 64, 1);
-insert into distribucion_silla values (4, 6, "Distribuccion 4", 6, 36, 1);
-insert into distribucion_silla values (5, 5, "Distribuccion 5", 5, 25, 1);
+insert into entrada values (1, "A", "3", 20000.0, 1);
+insert into entrada values (2, "A", "4", 20000.0, 1);
+insert into entrada values (3, "D", "4", 20000.0, 2);
+insert into entrada values (4, "A", "7", 20000.0, 3);
+insert into entrada values (5, "B", "4", 20000.0, 3);
